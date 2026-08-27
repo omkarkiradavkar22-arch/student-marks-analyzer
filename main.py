2bbef5b4cb77841fd2b5120ef21c4d8835ff1693
omkarkiradavkar22-arch <omkarkiradavkar22@gmail.com>
2026-08-27 20:10:30 +0530

    Add CGPA calculator

--- main.py
+++ main.py
@@ -2,53 +2,60 @@ import csv
 import os
 
 name = input("Enter student full name: ")
-subjects = ["Python", "Database", "Maths", "English", "Computer" ]
-marks = []
-for subject in subjects:
-    mark = float (input(f"Enter marks for {subjects}: "))
-    marks.append(mark)
-total = sum(marks)
-average = total / len(marks)
-if average >= 90:
-    grade = "A+"
-elif average >= 80:
-    grade = "A"
-elif average >= 70:
-    grade = "B"
-elif average >= 60:
-    grade = "B+"
-elif average >= 50:
-    grade = "D"
-else:
-    grade = "F"
-result = "PASS" if average >= 40 else "FAIL"
-print(f"Student Name :  {name}")
-print(f"Total Marks  :  {total}")
-print(f"Average      :  {average:.2f}")
-print(f"Grade        :  {grade}")
-print(f"Result       :  {result}")
 
-file_name = "student_results.csv"
+subjects = ["Python", "Database", "Maths", "English", "Computer"]
+credits = [4, 4, 3, 3, 4]
+
+grade_points = []
+
+for i, subject in enumerate(subjects):
+    point = float(input(f"Enter grade point for {subject} (0-10): "))
+    grade_points.append(point)
+
+total_credit_points = 0
+total_credits = sum(credits)
+
+for i in range(len(subjects)):
+    total_credit_points += grade_points[i] * credits[i]
+
+cgpa = total_credit_points / total_credits
+
+print("\n----- STUDENT RESULT -----")
+print(f"Student Name : {name}")
+print(f"CGPA         : {cgpa:.2f}")
+
+if cgpa >= 9:
+    result = "Excellent"
+elif cgpa >= 8:
+    result = "Very Good"
+elif cgpa >= 7:
+    result = "Good"
+elif cgpa >= 6:
+    result = "Average"
+elif cgpa >= 5:
+    result = "Pass"
+else:
+    result = "Fail"
+
+print(f"Result       : {result}")
+
+file_name = "student_cgpa.csv"
 file_exists = os.path.exists(file_name)
 
-with open(file_name , "a", newline ="") as file:
+with open(file_name, "a", newline="") as file:
     writer = csv.writer(file)
 
     if not file_exists:
         writer.writerow([
-            "Student name"
-            "Total Marks"
-            "Average"
-            "Grade"
-            "Results"
+            "Student Name",
+            "CGPA",
+            "Result"
         ])
 
     writer.writerow([
         name,
-        total,
-        round(average,2),
-        grade,
+        round(cgpa, 2),
         result
     ])
-    
-print("\nResult has been saved to student_result.csv")
+
+print("\nCGPA result has been saved to student_cgpa.csv")
